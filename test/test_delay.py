cc0e18168cf737c01248139c319f366a81f69333
Kronas <kronas.sw@gmail.com>
2016-04-21 14:08:14 +0300

    Refactor to Lua script

--- test/test_delay.py
+++ test/test_delay.py
@@ -1,3 +1,4 @@
+import json
 import tornado
 import tornadoredis
 from tornado.httpclient import AsyncHTTPClient
@@ -14,17 +15,33 @@ class TestDelay(AsyncTestCase):
         super(TestDelay, self).setUp()
 
     @tornado.testing.gen_test
-    def test_http_fetch(self):
+    def test_domain_delay(self):
         client = AsyncHTTPClient(self.io_loop)
-        response = yield client.fetch('http://127.0.0.1:5000/delay', method='POST', body='{"domain": "google.com"}')
-        # Test contents of response
+        response = yield client.fetch('http://127.0.0.1:5000/delay?domain=google.com', method='DELETE')
         self.assertIn('"success": true', response.body)
 
-        for x in xrange(1, 10):
-            response = yield client.fetch('http://127.0.0.1:5000/delay?domain=google.com&request_time=2.54')
-            # Test contents of response
-            self.assertIn('"timeout": 27.46', response.body)
+        response = yield client.fetch(
+            'http://127.0.0.1:5000/delay', method='POST', body='{"domain": "google.com", "duration": 300, "limit": 10}'
+        )
+        self.assertIn('"success": true', response.body)
 
-        response = yield client.fetch('http://127.0.0.1:5000/delay?domain=google.com&request_time=2.54')
-        # Test contents of response
-        self.assertIn('"timeout": 300', response.body)
+        response = yield client.fetch('http://127.0.0.1:5000/delay?domain=google.com&request_time=2.5')
+        body = json.loads(response.body)
+        self.assertTrue('timeout' in body)
+        self.assertTrue(float(body['timeout']))
+        timeout = float(body['timeout'])
+        self.assertEqual(27.5, round(timeout, 1))
+
+        response = yield client.fetch('http://127.0.0.1:5000/delay?domain=google.com&request_time=2')
+        body = json.loads(response.body)
+        self.assertTrue('timeout' in body)
+        self.assertTrue(float(body['timeout']))
+        timeout = float(body['timeout'])
+        self.assertEqual(55.5, round(timeout, 1))
+
+        response = yield client.fetch('http://127.0.0.1:5000/delay?domain=google.com&request_time=1.5')
+        body = json.loads(response.body)
+        self.assertTrue('timeout' in body)
+        self.assertTrue(float(body['timeout']))
+        timeout = float(body['timeout'])
+        self.assertEqual(84.0, round(timeout, 1))
--- throttler/handler.py
+++ throttler/handler.py
@@ -15,26 +15,32 @@ class DelayHandler(web.RequestHandler):
     @web.asynchronous
     @gen.engine
     def get(self, *args, **kwargs):
-        domain = self.get_argument('domain')
-        last_request_time = float(self.get_argument('request_time'))
-
-        domain_settings = yield gen.Task(self._prepare_domain_settings, domain)
-
-        key = 'next_request_time:%s' % domain
-
-        next_request_time = yield gen.Task(
-            self.db.eval,
-            self.calculate_next_request_time,
-            [key],
-            [domain_settings['request_timeout'], last_request_time, time.time()]
-        )
-
-        timeout = round(float(next_request_time) - time.time(), 3)
+        domain = self.get_argument('domain', None)
+        last_request_time = float(self.get_argument('request_time', 0.0))
 
         self.set_header('Content-Type', 'application/json')
-        self.finish(json.dumps({
-            'timeout': timeout if timeout > 0 else 0
-        }))
+        if domain:
+            domain_settings = yield gen.Task(self._prepare_domain_settings, domain)
+
+            key = 'next_request_time:%s' % domain
+
+            next_request_time = yield gen.Task(
+                self.db.eval,
+                self.calculate_next_request_time,
+                [key],
+                [domain_settings['request_timeout'], last_request_time, time.time()]
+            )
+
+            timeout = round(float(next_request_time) - time.time(), 3)
+
+            self.finish(json.dumps({
+                'timeout': timeout if timeout > 0 else 0
+            }))
+        else:
+            self.finish(json.dumps({
+                'timeout': 'error'
+            }))
+
 
     @web.asynchronous
     @gen.engine
@@ -45,18 +51,41 @@ class DelayHandler(web.RequestHandler):
         duration = float(data.get('duration', self.settings['duration']))
         limit = float(data.get('limit', self.settings['limit']))
 
-        request_timeout = round(duration / limit, 3)
+        self.set_header('Content-Type', 'application/json')
+        if domain:
+            request_timeout = round(duration / limit, 3)
 
-        result = yield gen.Task(self.db.hmset, 'limits:%s' % domain, {
-            'duration': duration,
-            'limit': limit,
-            'request_timeout': request_timeout
-        })
+            result = yield gen.Task(self.db.hmset, 'limits:%s' % domain, {
+                'duration': duration,
+                'limit': limit,
+                'request_timeout': request_timeout
+            })
+
+            self.finish(json.dumps({
+                'success': True if result else False
+            }))
+        else:
+            self.finish(json.dumps({
+                'success': False
+            }))
+
+    @web.asynchronous
+    @gen.engine
+    def delete(self, *args, **kwargs):
+        domain = self.get_argument('domain', None)
 
         self.set_header('Content-Type', 'application/json')
-        self.finish(json.dumps({
-            'success': True if result else False
-        }))
+        if domain:
+            yield gen.Task(self.db.hdel, 'limits:%s' % domain)
+            yield gen.Task(self.db.delete, 'next_request_time:%s' % domain)
+
+            self.finish(json.dumps({
+                'success': True
+            }))
+        else:
+            self.finish(json.dumps({
+                'success': False
+            }))
 
     @gen.engine
     def _prepare_domain_settings(self, domain, callback):
